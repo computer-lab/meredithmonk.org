751c9d5e418b437a4d3d649526f62bebb2d42538
Mark Neuburger <markneub@gmail.com>
2018-12-11 13:40:39 -0500

    Tweak footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -8,8 +8,8 @@ const Footer = ({ isDark }) => (
         <p>Meredith Monk/The House Foundation for the Arts</p>
         <p>260 West Broadway Suite 2</p>
         <p>New York, New York 10013</p>
-        <p>T | 212-904-1330</p>
-        <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#109;&#111;&#110;&#107;&#064;&#109;&#101;&#114;&#101;&#100;&#105;&#116;&#104;&#109;&#111;&#110;&#107;&#046;&#111;&#114;&#103;">monk@meredithmonk.org </a>
+        <p>☎ (212) 904-1330</p>
+        <p><a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#109;&#111;&#110;&#107;&#064;&#109;&#101;&#114;&#101;&#100;&#105;&#116;&#104;&#109;&#111;&#110;&#107;&#046;&#111;&#114;&#103;">monk@meredithmonk.org</a></p>
       </address>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous" />
