5ffdc6b7506231c1a89a69fcd823f5ba838f0fc4
Patrick Steadman <ptsteadman@gmail.com>
2018-12-12 16:13:22 -0500

    set proper ga code

--- frontend/config.js
+++ frontend/config.js
@@ -2,4 +2,4 @@ export const API_URL = process.env.NODE_ENV == 'production'
   ? 'https://wordpress-dot-wordpress-test-222422.appspot.com'
   : 'http://localhost:8080'
 // note that next-ga does not track on localhost
-export const GA_CODE = 'UA-foo-x'
+export const GA_CODE = 'UA-130942031-1'
