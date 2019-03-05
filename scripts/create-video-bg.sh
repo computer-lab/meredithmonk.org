a2971d710446bc46a483deac6804513883b13397
Patrick Steadman <ptsteadman@gmail.com>
2019-03-05 15:23:42 -0500

    change length of video background

--- scripts/create-video-bg.sh
+++ scripts/create-video-bg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # expects ffmpeg is installed, and a source video source.mov is in this directory
 
-start=00:00:10
-duration=00:00:30
+start=00:00:00
+duration=00:01:11
 
 ffmpeg -i source.mov -c:v h264 -ss $start -t $duration -async 1 -pix_fmt yuv420p ../frontend/static/background.mp4
 ffmpeg -i source.mov -c:v libtheora -ss $start -t $duration -async 1 ../frontend/static/background.ogv
