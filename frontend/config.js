1b3302a19337f3f244044784c9b2f51fafcb9476
Patrick Steadman <ptsteadman@gmail.com>
2019-03-27 19:04:06 -0400

    configure CORS urls for new env

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 export const API_URL = process.env.NODE_ENV == 'production'
-  ? 'https://wordpress-dot-wordpress-test-222422.appspot.com'
+  ? 'https://wordpress-dot-meredith-monk-website.appspot.com'
   : 'http://localhost:8080'
 // note that next-ga does not track on localhost
 export const GA_CODE = 'UA-130942031-1'
