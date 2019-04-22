f14b9c0bedaef57f0d53885fe307b287059421e3
Mark Neuburger <markneub@gmail.com>
2019-04-22 16:15:38 -0400

    Fix sorting and add year separation on calendar

--- frontend/components/CalendarEvents.js
+++ frontend/components/CalendarEvents.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react'
+import groupBy from 'lodash/groupBy'
 import compareAsc from 'date-fns/compare_asc'
 import compareDesc from 'date-fns/compare_desc'
 import isBefore from 'date-fns/is_before'
@@ -6,17 +8,20 @@ import urlParse from 'url-parse'
 
 const CalendarEvents = ({ events }) => {
   const now = new Date()
+  const getYear = ({ sort_date }) => sort_date.substring(0, 4)
   const pastEvents = events
     .filter(({ sort_date }) => isBefore(sort_date, now))
     .sort((a, b) => compareDesc(a.sort_date, b.sort_date))
+  const groupedPastEvents = groupBy(pastEvents, getYear)
   const upcomingEvents = events
     .filter(({ sort_date }) => !isBefore(sort_date, now))
-    .sort((a, b) => compareDesc(a.sort_date, b.sort_date))
+    .sort((a, b) => compareAsc(a.sort_date, b.sort_date))
+  const groupedUpcomingEvents = groupBy(upcomingEvents, getYear)
   const labels = ['Upcoming Events', 'Past Events']
 
   return (
     <div id="calendar-events">
-      {[upcomingEvents, pastEvents].map((eventsGroup, i) => (
+      {[groupedUpcomingEvents, groupedPastEvents].map((years, i) => (
         <React.Fragment key={labels[i]}>
           <h2>{labels[i]}</h2>
           <div id="calendar-events" className="table-responsive">
@@ -29,24 +34,31 @@ const CalendarEvents = ({ events }) => {
                 </tr>
               </thead>
               <tbody>
-                {eventsGroup.map(({
-                  name, date, location, link,
-                }) => (
-                  <tr key={name + date + location}>
-                    <td>
-                      <b>{name}</b>
-                      {link && (
-                        <>
-                          <br />
-                          <a className="event-link" href={link.url} target={link.target} title={link.title}>
-                            {urlParse(link.url).hostname}
-                          </a>
-                        </>
-                      )}
-                    </td>
-                    <td>{date}</td>
-                    <td>{location}</td>
-                  </tr>
+                {Object.keys(years).map(year => (
+                  <React.Fragment>
+                    <tr key={year} className="year-separator">
+                      <td colSpan="3">{year}</td>
+                    </tr>
+                    {years[year].map(({
+                      name, date, location, link,
+                    }, k) => (
+                      <tr key={year + k}>
+                        <td>
+                          <b>{name}</b>
+                          {link && (
+                            <>
+                              <br />
+                              <a className="event-link" href={link.url} target={link.target} title={link.title}>
+                                {urlParse(link.url).hostname}
+                              </a>
+                            </>
+                          )}
+                        </td>
+                        <td>{date}</td>
+                        <td>{location}</td>
+                      </tr>
+                    ))}
+                  </React.Fragment>
                 ))}
               </tbody>
             </table>
