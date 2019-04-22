385433311150f545d95a1d2f27e66a7eae098ace
Mark Neuburger <markneub@gmail.com>
2019-04-22 13:38:43 -0400

    Fix gif display, normalize image sizes

--- frontend/components/RepertoryWorks.js
+++ frontend/components/RepertoryWorks.js
@@ -8,22 +8,24 @@ const RepertoryWorks = ({ repertoryWorks }) => (
   <div id="works-gallery">
     {sortBy(repertoryWorks, 'menu_order').map((work) => {
       const isCurrentRepertoryWork = work.acf.is_current_repertory_work
-      const imgSrc = safeGet(work, "['_embedded']['wp:featuredmedia'][0]['media_details']['sizes']['thumbnail']['source_url']") || 'https://via.placeholder.com/400x400'
+      const imgSrc = safeGet(work, "['_embedded']['wp:featuredmedia'][0]['source_url']") || 'https://via.placeholder.com/400x400'
 
       return (
         <div
           className={classNames(['work', 'card', isCurrentRepertoryWork && 'current'])}
           key={work.slug}
         >
-          <Link
-            prefetch
-            as={`/current-repertory/${work.slug}/`}
-            href={work.link}
-          >
-            <a>
-              <img className="card-img-top" src={imgSrc} alt={work.title.rendered} />
-            </a>
-          </Link>
+          <div className="card-img-wrap">
+            <Link
+              prefetch
+              as={`/current-repertory/${work.slug}/`}
+              href={work.link}
+            >
+              <a>
+                <img className="card-img-top" src={imgSrc} alt={work.title.rendered} />
+              </a>
+            </Link>
+          </div>
           <div className="card-body">
             <div className="work-name card-text">
               <Link
