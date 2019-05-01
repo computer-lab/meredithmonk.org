6b16961e9593fefac11c3aaa884d0ebde0f403ae
Mark Neuburger <markneub@gmail.com>
2019-05-01 11:52:26 -0400

    Use thumbnails again, use CSS to stretch in case undersized images (WP does not upscale to square)

--- frontend/components/RepertoryWorks.js
+++ frontend/components/RepertoryWorks.js
@@ -8,24 +8,21 @@ const RepertoryWorks = ({ repertoryWorks }) => (
   <div id="works-gallery">
     {sortBy(repertoryWorks, 'menu_order').map((work) => {
       const isCurrentRepertoryWork = work.acf.is_current_repertory_work
-      const imgSrc = safeGet(work, "['_embedded']['wp:featuredmedia'][0]['source_url']") || 'https://via.placeholder.com/400x400'
+      const imgSrc = safeGet(work, "['_embedded']['wp:featuredmedia'][0]['media_details']['sizes']['thumbnail']['source_url']") || 'https://via.placeholder.com/400x400'
 
       return (
         <div
           className={classNames(['work', 'card', isCurrentRepertoryWork && 'current'])}
           key={work.slug}
         >
-          <div className="card-img-wrap">
-            <Link
-              prefetch
-              as={`/repertory/${work.slug}/`}
-              href={work.link}
-            >
-              <a>
-                <img className="card-img-top" src={imgSrc} alt={work.title.rendered} />
-              </a>
-            </Link>
-          </div>
+          <Link
+            prefetch
+            as={`/repertory/${work.slug}/`}
+            href={work.link}
+          >
+            {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+            <a className="card-img-top" title={work.title.rendered} style={{ backgroundImage: `url(${imgSrc}` }} />
+          </Link>
           <div className="card-body">
             <div className="work-name card-text">
               <Link
